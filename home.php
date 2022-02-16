71dfeb44fd70ea0f6d6e00b935852d3d6acbdb81
Sprighter <88900421+bibek32@users.noreply.github.com>
2022-02-16 14:33:46 +0545

    Create home.php

---
+++ home.php
@@ -0,0 +1,39 @@
+<head>
+    <title>Bibek Silwal</title>
+    <H1
+     style="background-color: blue;"> 
+                    <font color="#e8e8e8">
+               <b>Bibek Silwal</b>
+            </font>
+            
+    </H1>        
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+        #RU{
+            text-decoration: none;
+            ul
+            {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+
+             li {
+                float: left;
+            }
+
+            li a
+            {
+                display: block;
+                padding: 8px;
+                background-color: #dddddd;
+            }
+          }
+        </style>
+    <div class="navbar"> 
+            <a id="RU" class="active" href="pages=home.php"><i class="fa fa-fw fa-home"></i> Home</a>
+            <a id="RU" href="index.php?pages=contact.php"><i class="fa fa-fw fa-search"></i> Contact</a>
+            <a id="RU" href="index.php?pages=about.php"><i class="fa fa-fw fa-envelope"></i> About</a>
+    </div>
+</head>
